e4f7f510afcab64df1491cbf9b3cda5fcc27e872
yorman-129 <yorman_tobon82201@elpoli.edu.co>
2023-07-18 17:34:17 -0500

    feat: change in request

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,16 +1,17 @@
 import { React, useState } from "react";
 import Card from './card/Card'
 const Data = () => {
+
   const [data, setData] = useState([])
-  const [getData, setGet]=useState(false)
+  const [mensaje, setMensaje] = useState([])
 
 
-  const request = async () => {
-    const url = 'https://27b4-2800-e6-4000-49d6-7197-eab-919f-e1d5.ngrok-free.app/api/get-articles';
+  const request = async (msje) => {
+    const url = 'https://bbdd-2800-e6-4000-49d6-603c-684f-9133-7ab5.ngrok-free.app/api/get-articles';
     const apiKey = "BQ-csVdQijnxsHHsIscNzqUDomcDoiLjyGMLZkzBPTxATHtehVUfLfisAwlRkhjieNBZxcjlGoqTIeWbA";
-    const dropdownValue = "movilidad";
+    const dropdownValue = msje;
     const requestData = new FormData();
-
+    console.log(msje)
     requestData.append('dropdown', dropdownValue);
     const response = await fetch(url, {
       method: 'POST',
@@ -18,45 +19,59 @@ const Data = () => {
         'Content-Type': 'application/json',
         'Authorization': apiKey
       },
-      body: requestData
+      body: requestData,
+      mode: 'cors' 
                                       })
 
     const responseData = await response.json();
     const newData = [];
 
       for (let index = 0; index < 20; index++) {
-
         const obj =
         {
           "title": responseData.title[index],
-          "images": responseData.images[index][0],
+          "images": responseData?.images[index][0],
           "summary": responseData.summary[index],
           "keywords": responseData.keywords[index],
           "link": responseData.link[index]
         }
         newData.push(obj)
-        console.log(obj);
-        
       }
-      console.log("finExmaple\n");
+      
       setData([...data,...newData])
       setGet(true)
     };
 
-  const handleRequest = () => request()
+    const handleMensaje=(e)=> {
+      setMensaje(
+        {...mensaje, value:e.target.value}
+        )}
+
+    const handleRequest = () =>
+    { 
+      request(mensaje.value)
+    }
+
+    const saveSeñal = (tittle,description,imagen,tags,link)=>{
+
+
+    }
+
 
   return (
     <>
       <label htmlFor="">Ingresa la palabra</label>
-      <input type="text" />
+      <input type="text" onChange={handleMensaje}/>
       <input type="submit" value="Enviar" onClick={handleRequest} />
-      {console.log(data,"\n-------------------------------------------\n")}
+      <div>
+
       { data && (
         data.map((dataE) =>{
-          return <Card tittle={dataE.tittle} description={dataE.summary} imagen={dataE.images} tags={dataE.keywords} link={dataE.link} />;
+          return <Card onSubmit={saveSeñal} tittle={dataE.tittle} description={dataE.summary} imagen={dataE.images} tags={dataE.keywords} link={dataE.link} />;
         })
 
       )}
+      </div>
     </>
   );
 }
--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 
-const Card=({tittle,description,imagen,tags,link})=>{
+const Card=({tittle,description,imagen,tags,link,saveSeñal})=>{
+
+    const handleSeñal= ()=>{
+        saveSeñal(tittle,description,imagen,tags,link)
+    }
 
     return(
         <>
         <div className="card-container">
             <h2>{tittle}</h2>
             <img src={imagen} width="250" height="250"/>
-            <p>{description}</p>
+            <p>{description}<br/>
+            <a href={link}>Link</a></p>
+            <input type="submit" value="Enviar Señal" onClick={handleSeñal}/>
             <div className="tags">
                 {tags}
             </div>
-            <input type="submit" value="Enviar Señal" />
         </div>
         
         </>
