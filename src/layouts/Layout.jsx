2c910af14e0ba5e09ea2c7b6486fa349b9bb3d07
Juan Pablo RP <jpabloroldanp@gmail.com>
2023-07-22 17:15:57 -0500

    feat: toastify

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,25 @@
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Layout = () => {
 	return (
 		<div>
 			<Navbar />
 			<div>
+				<ToastContainer
+					position="top-right"
+					autoClose={5000}
+					hideProgressBar={false}
+					newestOnTop={false}
+					closeOnClick
+					rtl={false}
+					pauseOnFocusLoss
+					draggable
+					pauseOnHover
+					theme="dark"
+				/>
 				<Outlet />
 			</div>
 		</div>
--- src/pages/NoticiaDetalle.jsx
+++ src/pages/NoticiaDetalle.jsx
@@ -1,27 +1,29 @@
+import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import noticias from '../data/noticias.json';
+//import noticias from '../data/noticias.json';
 import { HiArrowLeft } from 'react-icons/hi2';
 
 const NoticiaDetalle = () => {
 	const { id } = useParams();
 
-	const [info, setInfor] = useState([])
+	const [info, setInfor] = useState([]);
 	useEffect(() => {
 		const requestOptions = {
-		  method: 'GET',
-		  redirect: 'follow'
+			method: 'GET',
+			redirect: 'follow',
 		};
-	
-		fetch("http://localhost:8080/news/findAll", requestOptions)
-		  .then(response => response.json())
-		  .then(result => setInfor(result))   
-		  .catch(error => console.log('error', error));
-	  }, []);
-	  
-	const news = info.map(({ noticia }) => noticia);
-	const noticia = news.find((noticia) => noticia.id === Number(id));
 
-	const tagsString = noticia.tags.join(', ');
+		fetch('http://localhost:8080/news/findAll', requestOptions)
+			.then((response) => response.json())
+			.then((result) => setInfor(() => result))
+			.catch((error) => console.log('error', error));
+	}, []);
+
+	console.log(info);
+	const news = info.map((noticia) => noticia);
+	const noticia = news.find((noticia) => noticia?.id === Number(id));
+
+	//const tagsString = noticia?.tags.join(', ');
 
 	if (!noticia) {
 		return (
@@ -35,7 +37,7 @@ const NoticiaDetalle = () => {
 	}
 
 	return (
-		<main className="max-w-4xl m-auto py-6 flex flex-col justify-center items-center border border-gray-800 rounded-md my-1 md:my-3 md:px-4">
+		<main className="max-w-4xl m-auto py-6 flex flex-col justify-center items-center border-4  border-gray-800 bg-gray-800 rounded-md my-1 md:my-3 md:px-4">
 			<Link to={'/noticias'} className="my-2  md:self-start">
 				<HiArrowLeft className="w-9 h-9 md:w-10 md:h-10" />
 			</Link>
@@ -49,13 +51,14 @@ const NoticiaDetalle = () => {
 					height={500}
 					className="rounded-md"
 				/>
-				<p className="text-center px-2 py-4 opacity-60">Tags: {tagsString}</p>
-				<p className="md:px-6">{noticia.description}</p>
+				<p className="text-center px-2 py-4 opacity-60">Tags: {noticia.tags}</p>
+				<span className="text-center text-xl py-2 font-semibold">Resumen noticia</span>
+				<p className="md:px-6 text-lg">{noticia.description}</p>
 				<a
 					href={noticia.link}
 					target="_blank"
 					rel="noreferrer"
-					className="hover:text-neutral-400 pt-4"
+					className=" mt-5 px-4 py-3 rounded-md bg-neutral-900 hover:bg-neutral-950 focus:ring-2 focus:ring-blue-600 shadow-md shadow-black -translate-y-1 hover:shadow-none hover:translate-y-0"
 				>
 					Ir al sitio web
 				</a>
