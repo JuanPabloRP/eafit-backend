552d114262926d4d05240f19259bd8aeef79df48
Juan Pablo RP <jpabloroldanp@gmail.com>
2023-07-23 12:30:20 -0500

    feat: ahora se puede eliminar noticias al igual que se pueden buscar

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,12 +1,47 @@
 import { Link } from 'react-router-dom';
+import { HiTrash } from 'react-icons/hi2';
+import { useState } from 'react';
 
 const CardItem = ({
 	noticia: { id, title, tags, image, description, link, tipoNoticia },
+	deleteNoticia,
 }) => {
-	console.log({ id, title, tags, image, description, link, tipoNoticia });
+	console.log({
+		id,
+		title,
+		tags,
+		image,
+		description,
+		link,
+		tipoNoticia,
+		deleteNoticia,
+	});
+
+	const [open, setOpen] = useState(false);
+
+	const handleModal = () => {
+		setOpen(!open);
+	};
 
 	return (
-		<article className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg shadow-gray-800 dark:bg-gray-800 dark:border-gray-700 m-2 ">
+		<article className="relative max-w-sm  border border-gray-200 rounded-lg shadow-lg shadow-gray-800 dark:bg-gray-800 dark:border-gray-700 m-2 hover:border-gray-600 hover:shadow-gray-600">
+			<button
+				data-modal-target="defaultModal"
+				data-modal-toggle="defaultModal"
+				className="absolute w-full flex justify-end pt-2 pr-2"
+				onClick={handleModal}
+			>
+				<HiTrash className="absolute bg-gray-200  m-1 p-1 text-red-600 w-8 h-8 hover:text-white hover:bg-red-600 rounded-full" />
+			</button>
+
+			<Modal
+				open={open}
+				setOpen={setOpen}
+				title={title}
+				id={id}
+				deleteNoticia={deleteNoticia}
+			/>
+
 			<figure className="flex justify-center items-center rounded-t-lg">
 				<img
 					className="w-full h-full rounded-t-lg"
@@ -45,3 +80,57 @@ const CardItem = ({
 };
 
 export default CardItem;
+
+const Modal = ({ open, setOpen, title, id, deleteNoticia }) => {
+	const handleClose = () => setOpen(false);
+
+	const handleDelete = () => {
+		deleteNoticia(id);
+		handleClose();
+	};
+
+	return (
+		<article
+			className={`${
+				open ? 'flex' : 'hidden'
+			} shadow-lg shadow-gray-800 bg-gray-800 border border-gray-600 rounded-md fixed top-0 left-0 right-0 max-w-md  mx-auto m-4  z-50   p-4 overflow-x-hidden flex-col mt-12`}
+		>
+			<header className="py-2">
+				<h2 className="text-red-600 font-semibold text-2xl">
+					Eliminar permanentemente
+				</h2>
+			</header>
+			<main>
+				<p>¿Estás seguro que deseas eliminar: {title}?</p>
+			</main>
+			<footer className="flex justify-between items-center py-2">
+				<button
+					type="button"
+					onClick={handleDelete}
+					className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+				>
+					Eliminar
+				</button>
+				<button
+					type="button"
+					onClick={handleClose}
+					className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+				>
+					Cerrar
+				</button>
+			</footer>
+		</article>
+	);
+};
+
+/*
+	
+<!-- Modal toggle -->
+<button data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+  Toggle modal
+</button>
+
+<!-- Main modal -->
+
+
+ */
--- src/pages/Noticias.jsx
+++ src/pages/Noticias.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
 import CardItem from '../components/CardItem';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import { toast } from 'react-toastify';
 
 const Noticias = () => {
 	const [isLoading, setIsLoading] = useState(true);
 	const [error, setError] = useState(false);
-
 	const [info, setInfor] = useState([]);
-	useEffect(() => {
+	const [text, setText] = useState('');
+
+	const getInfo = () => {
 		const requestOptions = {
 			method: 'GET',
 			redirect: 'follow',
@@ -23,67 +27,153 @@ const Noticias = () => {
 				setError(true);
 			})
 			.finally(() => setIsLoading(false));
+	};
+
+	useEffect(() => {
+		getInfo();
 	}, []);
+
+	const deleteNoticia = (id) => {
+		const requestOptions = {
+			method: 'DELETE',
+			redirect: 'follow',
+		};
+
+		fetch(`http://localhost:8080/news/delete/${id}`, requestOptions)
+			.then((response) => response.text())
+			.then((result) => {
+				console.log(result);
+				toast.success('La noticia se ha eliminado con éxito', {
+					position: 'top-right',
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'dark',
+				});
+			})
+			.catch((error) => {
+				console.log('error', error);
+				toast.error('Error al eliminar la noticia', {
+					position: 'top-right',
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: true,
+					draggable: true,
+					progress: undefined,
+					theme: 'dark',
+				});
+			})
+			.finally(getInfo());
+	};
 
 	return (
 		<main className="h-full w-full">
 			<h2 className="text-3xl mb-4 md:text-5xl font-bold text-center py-4">
 				Ver noticias
 			</h2>
+
+			<SearchInput isLoading={isLoading} setText={setText} />
 			<main>
 				{isLoading ? (
 					<Loading />
 				) : (
-					<>{error ? <Error /> : <Succesfully info={info} />}</>
+					<>
+						{error ? (
+							<Error />
+						) : (
+							<Succesfully
+								info={info}
+								deleteNoticia={deleteNoticia}
+								text={text}
+							/>
+						)}
+					</>
 				)}
 			</main>
 		</main>
 	);
 };
 
-const Error = () => {
-	return (
-		<p className="text-red-600 text-center text-xl font-semibold">
-			Error al obtener los datos, intente más tarde o comuníquese con el
-			adminstrador
-		</p>
-	);
-};
+const Succesfully = ({ info, deleteNoticia, text }) => {
+	//console.log(info);
+	//console.log(text);
 
-const Succesfully = ({ info }) => {
 	return (
-		<section className="flex flex-wrap justify-center items-center">
-			{info
-				? info.map((item) => <CardItem key={item.id} noticia={item} />)
-				: console.log('error')}
+		<section className="flex flex-wrap justify-center items-start">
+			{info ? (
+				info
+					.filter((item) =>
+						text?.length > 0
+							? item?.title?.toLowerCase()?.includes(text?.toLowerCase()) ||
+							  item?.tipoNoticia?.toLowerCase()?.includes(text?.toLowerCase())
+							: item
+					)
+					.map((item) => (
+						<CardItem
+							key={item.id}
+							noticia={item}
+							deleteNoticia={deleteNoticia}
+						/>
+					))
+			) : (
+				<Error />
+			)}
 		</section>
 	);
 };
 
-const Loading = () => {
+const SearchInput = ({ setText }) => {
+	const handleMensaje = (e) => {
+		e.preventDefault();
+		setText(e.target.value);
+	};
+
 	return (
-		<section
-			role="status"
-			className="grid place-items-center h-full w-full mt-4"
+		<form
+			className="max-w-2xl m-auto mt-4 md:my-8 "
+			onClick={(e) => e.preventDefault()}
+			onSubmit={(e) => e.preventDefault()}
 		>
-			<svg
-				aria-hidden="true"
-				className="w-12 h-12 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
-				viewBox="0 0 100 101"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
+			<label
+				htmlFor="default-search"
+				className="mb-2 text-sm font-medium  sr-only text-white"
 			>
-				<path
-					d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-					fill="currentColor"
+				Buscar
+			</label>
+			<div className="relative">
+				<div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+					<svg
+						className="w-4 h-4 dark:text-gray-400"
+						aria-hidden="true"
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 20 20"
+					>
+						<path
+							stroke="currentColor"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+						/>
+					</svg>
+				</div>
+
+				<input
+					type="text"
+					id="default-search"
+					className="block w-full p-4 pl-10 text-sm  border  rounded-lg bg-gray-800 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500 focus-visible:border-none "
+					placeholder="Busque por el título o el tipo de la noticia..."
+					onChange={handleMensaje}
+					autoComplete="off"
+					required
 				/>
-				<path
-					d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-					fill="currentFill"
-				/>
-			</svg>
-			<span className="sr-only">Cargando...</span>
-		</section>
+			</div>
+		</form>
 	);
 };
 
